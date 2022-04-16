3c126bb387b7df1e299104a13ebb7538661f6603
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-16 18:36:06 +0300

    Added data handler.

---
+++ src/main/java/handler/DataHandler.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2022.
+ * Created by Roman on 16.04.2022.
+ */
+
+package handler;
+
+public class DataHandler
+{
+    private String[] oneDeclination;
+    private String[] twoDeclination;
+    private String[] thousandDeclination;
+    private String[] degreeDeclination;
+    private String[] numberDegrees;
+    private String[] units;
+    private String[] decimalBetweenTenAndTwenty;
+    private String[] decimal;
+    private String[] hundreds;
+
+    public String[] getOneDeclination()
+    {
+        return oneDeclination;
+    }
+
+    public void setOneDeclination(String[] oneDeclination)
+    {
+        this.oneDeclination = oneDeclination;
+    }
+
+    public String[] getTwoDeclination()
+    {
+        return twoDeclination;
+    }
+
+    public void setTwoDeclination(String[] twoDeclination)
+    {
+        this.twoDeclination = twoDeclination;
+    }
+
+    public String[] getThousandDeclination()
+    {
+        return thousandDeclination;
+    }
+
+    public void setThousandDeclination(String[] thousandDeclination)
+    {
+        this.thousandDeclination = thousandDeclination;
+    }
+
+    public String[] getDegreeDeclination()
+    {
+        return degreeDeclination;
+    }
+
+    public void setDegreeDeclination(String[] degreeDeclination)
+    {
+        this.degreeDeclination = degreeDeclination;
+    }
+
+    public String[] getNumberDegrees()
+    {
+        return numberDegrees;
+    }
+
+    public void setNumberDegrees(String[] numberDegrees)
+    {
+        this.numberDegrees = numberDegrees;
+    }
+
+    public String[] getUnits()
+    {
+        return units;
+    }
+
+    public void setUnits(String[] units)
+    {
+        this.units = units;
+    }
+
+    public String[] getDecimalBetweenTenAndTwenty()
+    {
+        return decimalBetweenTenAndTwenty;
+    }
+
+    public void setDecimalBetweenTenAndTwenty(String[] decimalBetweenTenAndTwenty)
+    {
+        this.decimalBetweenTenAndTwenty = decimalBetweenTenAndTwenty;
+    }
+
+    public String[] getDecimal()
+    {
+        return decimal;
+    }
+
+    public void setDecimal(String[] decimal)
+    {
+        this.decimal = decimal;
+    }
+
+    public String[] getHundreds()
+    {
+        return hundreds;
+    }
+
+    public void setHundreds(String[] hundreds)
+    {
+        this.hundreds = hundreds;
+    }
+}
