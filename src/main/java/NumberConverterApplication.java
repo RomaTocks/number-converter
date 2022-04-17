69afadcf2d985848e1877352fb43bba7034e6e9a
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-17 04:28:03 +0300

    Added scanning of numbers.

--- src/main/java/NumberConverterApplication.java
+++ src/main/java/NumberConverterApplication.java
@@ -1,11 +1,37 @@
+import converter.Convertable;
 import converter.NumberConverter;
+import reader.DataReader;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class NumberConverterApplication {
-    public static void main(String[] args) {
-        String input = "134345";
-        BigInteger bigInteger = new BigInteger(input);
-        System.out.println(NumberConverter.convertNumberIntoWords(bigInteger));
+    public static void main(String[] args)
+    {
+        DataReader reader = new DataReader();
+        try
+        {
+            reader.read();
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите число: ");
+                try
+                {
+                    BigInteger value = scanner.nextBigInteger();
+                    Convertable convertable = new NumberConverter(reader.getConfiguredData());
+                    String answer = convertable.convertNumberIntoWords(value);
+                    System.out.println("Ответ: " + answer);
+                }
+                catch (Exception exception)
+                {
+                    System.out.println("Некорректное число!");
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Не удалось прочитать конфигурацию конвертера.");
+        }
     }
 }
